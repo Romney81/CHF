f36cb1d48c17e9961e4322ec18c0ecc8b8a2a188
rodcox89 <neddam12>
2015-04-09 16:05:31 -0600

    navbar re-styled

--- homepage/views/index.py
+++ homepage/views/index.py
@@ -3,15 +3,36 @@ import django.contrib.auth
 from django.contrib.auth import logout
 from django_mako_plus.controller import view_function
 from django_mako_plus.controller.router import get_renderer
+import homepage.models as hmod
 
 templater = get_renderer('homepage')
 
 @view_function
 def process_request(request):
-  template_vars = {
-  }
 
-  return templater.render_to_response(request, 'index.html', template_vars)
+    #empty params
+    params = {}
+
+    #get all events
+    events = hmod.Event.objects.all()
+
+    #month and day lists
+    month = []
+    day = []
+
+    #for loop to get month and day
+    for event in events:
+        newmonth = event.start_date.strftime('%b')
+        newday = event.start_date.strftime('%d')
+        month.append(newmonth)
+        day.append(newday)
+
+    #pass event, month and day to festivals.html
+    params['months'] = month
+    params['days'] = day
+    params['events'] = events
+
+    return templater.render_to_response(request, 'index.html', params)
 
 
 @view_function
